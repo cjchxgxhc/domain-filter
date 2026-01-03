9f3d68214ffe400aac88a0f40085ca82d6d9aa04
cjchxgxhc <ww13796820528@163.com>
2026-01-03 20:18:06 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -62,6 +62,7 @@ BLACKLIST_CONFIG = {
 WHITELIST_CONFIG = {
     "ads": [
         "https://raw.githubusercontent.com/cjchxgxhc/domain-filter/refs/heads/main/rules/ads_white.txt",
+        "https://raw.githubusercontent.com/qq5460168/Who520/refs/heads/main/white.txt",
         "https://raw.githubusercontent.com/217heidai/adblockfilters/refs/heads/main/rules/white.txt"
     ],
     "HaGeZi's Pro++ mini Blocklist": [
