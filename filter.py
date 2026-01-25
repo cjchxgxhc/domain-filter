568e34c47adad5f53adc5645739e3a955ef0cea6
cjchxgxhc <ww13796820528@163.com>
2026-01-25 22:09:53 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -25,19 +25,20 @@ CONFIG = {
     "ads": {
         "blocklist": [
             "https://raw.githubusercontent.com/cjchxgxhc/domain-filter/refs/heads/main/rules/ads.txt",
-           # "https://raw.githubusercontent.com/Aethersailor/adblockfilters-modified/refs/heads/main/rules/adblockdnslite.txt",
+            "https://raw.githubusercontent.com/Aethersailor/adblockfilters-modified/refs/heads/main/rules/adblockdnslite.txt",
             "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
             "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt",
             "https://raw.githubusercontent.com/lingeringsound/10007_auto/refs/heads/master/configure/%E8%87%AA%E5%AE%9A%E4%B9%89.prop",
-           # "https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/refs/heads/adlist-maker/scripts/origin-files/anti-ad-origin-block.txt",
-           # "https://raw.githubusercontent.com/Cats-Team/AdRules/refs/heads/script/mod/rules/dns-rules.txt",
-           # "https://raw.githubusercontent.com/qq5460168/666/master/dns.txt",
+            "https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/refs/heads/adlist-maker/scripts/origin-files/anti-ad-origin-block.txt",
+            "https://raw.githubusercontent.com/Cats-Team/dns-filter/main/abp.txt",
+            "https://raw.githubusercontent.com/qq5460168/Who520/refs/heads/main/adhosts.txt",
             "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/domains/native.oppo-realme.txt",
             "https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/adblock/native.xiaomi.txt"
         ],
         "whitelist": [
             "https://raw.githubusercontent.com/cjchxgxhc/domain-filter/refs/heads/main/rules/ads_white.txt",
             "https://raw.githubusercontent.com/qq5460168/Who520/refs/heads/main/white.txt",
+            "https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-for-clash.yaml",
           #  "https://raw.githubusercontent.com/neodevpro/neodevhost/refs/heads/master/allow",
             "https://raw.githubusercontent.com/217heidai/adblockfilters/refs/heads/main/rules/white.txt"
         ],
